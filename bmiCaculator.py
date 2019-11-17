f1790778fdde21aa2fa9ad3eafbdc5deecac6c38
ray-abel12 <rayabel12@outlook.com>
2019-11-17 23:44:19 +0100

    commit

---
+++ bmiCaculator.py
@@ -0,0 +1,6 @@
+weight = input('Enter weight in kilograms ')
+height = input('Enter height in meters ')
+weight = int(weight)
+height = int(height)
+bmi = weight/height * height
+print(f'the body mass index is {bmi}')
